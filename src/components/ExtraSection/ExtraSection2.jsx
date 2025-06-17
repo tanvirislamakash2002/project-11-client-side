7ea34a77e1ce202ef86fcc577273970e72083d03
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2025-06-17 17:16:59 +0600

    extrasection 2 functionallytiy done

--- src/components/ExtraSection/ExtraSection2.jsx
+++ src/components/ExtraSection/ExtraSection2.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ArticleCard from '../ArticleCard';
 
 const ExtraSection2 = ({allArticlesData}) => {
-    console.log(allArticlesData)
+
+
+        const sortedByLikes = [...allArticlesData].sort((a,b)=>(b.likedBy?.length || 0)-a.likedBy?.length||0);
+        const top3MostLiked =sortedByLikes.slice(0,3)
+
     return (
         <div>
-            2
+            {
+                top3MostLiked.map(articleData=>
+                    <ArticleCard articleData={articleData}></ArticleCard>
+                )
+            }
         </div>
     );
 };
